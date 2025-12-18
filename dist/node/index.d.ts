99e28c52348a6e52fdb70968be3364176afa6247
Kittizz <62899732+kittizz@users.noreply.github.com>
2025-12-19 00:47:59 +0700

    chore: remove dist/ from .gitignore to track build artifacts chore: update .opencommitignore to use wildcard pattern for lib files

---
+++ dist/node/index.d.ts
@@ -0,0 +1,7 @@
+//#region src/node/lib.d.ts
+declare function randomString$1(length: number): string;
+//#endregion
+//#region src/node/index.d.ts
+declare const randomString: typeof randomString$1;
+//#endregion
+export { randomString };
